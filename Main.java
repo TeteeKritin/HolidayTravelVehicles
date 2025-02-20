7cfcc8dfd7210d68b13166691848775459310307
Kritin Chitchobtham <kritin.te@gmail.com>
2025-02-21 00:03:27 +0700

    First

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a new Vehicle object
+        Vehicle vehicle = new Vehicle("Toyota", "Camry", 2023, 25000.0);
+
+        // Display vehicle information
+        System.out.println(vehicle);
+    }
+}
